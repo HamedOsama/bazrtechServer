4beddf2e31c2cf1b1b1f35e4e766f120688b8521
Hamed Osama <hamedosama19@gmail.com>
2023-01-27 18:35:23 +0200

    update calculator

--- src/controllers/calculator.controller.js
+++ src/controllers/calculator.controller.js
@@ -3,16 +3,16 @@ const Shipping = require("../model/shipping");
 
 const calculateShipping = async (req, res, next) => {
   try {
-    const { country, price, category, weight } = req.body;
+    const { country, price, category, weight, quantity = 1 } = req.body;
     if (!country || !price || !category || !weight)
       return next(ServerError.badRequest(400, 'جميع الحقول مطلوبة'));
-    const shippingData = await  Shipping.findOne({ country });
+    const shippingData = await Shipping.findOne({ country });
     if (!shippingData)
       return next(ServerError.badRequest(400, 'غير متوفر شحن لهذه المنطقة'));
     const multiply = ['accessories', 'shoes', 'bags', 'electronics', 'homeApplicants', 'furniture']
     let total = 0
     if (multiply.includes(category)) {
-      total = price +  shippingData[category];
+      total = price + shippingData[category];
     }
     if (category === 'clothes') {
       if (weight > 0 && weight < 1)
@@ -24,9 +24,9 @@ const calculateShipping = async (req, res, next) => {
       if (weight >= 3 && weight < 4)
         total = price + shippingData.clothes.four
       if (weight >= 4 && weight < 10)
-        total = price +  (weight * shippingData.clothes.five)
+        total = price + (weight * shippingData.clothes.five)
       if (weight >= 10)
-        total = price + ( weight * shippingData.clothes.six)
+        total = price + (weight * shippingData.clothes.six)
     }
     if (category === 'makeUp') {
       if (weight > 0 && weight < 0.5)
@@ -42,7 +42,15 @@ const calculateShipping = async (req, res, next) => {
       if (weight >= 4 && weight < 5)
         total = price + shippingData.makeUp.five
       if (weight >= 5)
-        total = price +  (weight * shippingData.makeUp.six)
+        total = price + (weight * shippingData.makeUp.six)
+    }
+    if (category === 'carParts') {
+      if (weight > 0 && weight < 1)
+        total = price + (weight * shippingData.carParts.one)
+      if (weight >= 1 && weight < 2)
+        total = price + (weight * shippingData.carParts.two)
+      if (weight >= 2)
+        total = price + (weight * shippingData.carParts.three)
     }
     if (category === 'foodSupplements') {
       if (weight > 0 && weight < 0.5)
@@ -66,11 +74,11 @@ const calculateShipping = async (req, res, next) => {
       if (weight >= 2 && weight < 3)
         total = price + shippingData.iron.three
       if (weight >= 3 && weight < 5)
-        total =  price +  (weight * shippingData.iron.four)
+        total = price + (weight * shippingData.iron.four)
       if (weight >= 5 && weight < 10)
-        total = price +  (weight * shippingData.iron.five)
+        total = price + (weight * shippingData.iron.five)
       if (weight >= 10)
-        total = price +  (weight * shippingData.iron.six)
+        total = price + (weight * shippingData.iron.six)
     }
     if (category === 'other') {
       if (weight > 0 && weight < 1)
@@ -82,14 +90,15 @@ const calculateShipping = async (req, res, next) => {
       if (weight >= 3 && weight < 5)
         total = price + shippingData.other.four
       if (weight >= 5 && weight < 10)
-        total =  price + (weight * shippingData.other.five)
+        total = price + (weight * shippingData.other.five)
       if (weight >= 10)
-        total = price +  (weight * shippingData.other.six)
+        total = price + (weight * shippingData.other.six)
     }
+    total *= quantity;
     res.status(200).json({
-      ok : true,
-      status : 200 ,
-      message : 'succeed',
+      ok: true,
+      status: 200,
+      message: 'succeed',
       price,
       total
     })
